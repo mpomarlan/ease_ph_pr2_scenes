f0f701f150329ffe46c20bfefd639f8f74f3df38
Mihai Pomarlan <mpomarlan@yahoo.co.uk>
2018-09-12 10:00:21 +0200

    Reverted changes to nodding (they were pushed by mistake anyway).

--- nodding_signal.py
+++ nodding_signal.py
@@ -6,15 +6,15 @@ from geometry_msgs.msg import Twist
 import numpy
 
 def talker():
-    #pub = rospy.Publisher('/pr2/head_tilt_joint/cmd_vel', Twist, queue_size=10)
-    pub = rospy.Publisher('/r_shoulder_pan_velocity_controller/command', Twist, queue_size=10)
+    pub = rospy.Publisher('/pr2/head_tilt_joint/cmd_vel', Twist, queue_size=10)
+    #pub = rospy.Publisher('/r_shoulder_pan_velocity_controller/command', Twist, queue_size=10)
     rospy.init_node('nodding_signal', anonymous=True)
     f = 10.0
     rate = rospy.Rate(f) # 10hz
     t = 0.0
     while not rospy.is_shutdown():
         da_twist = Twist()
-        da_twist.angular.z = 10#10*numpy.sin(2*3.14159*0.2*t)
+        da_twist.angular.z = 10*numpy.sin(2*3.14159*0.2*t)
         pub.publish(da_twist)
         rate.sleep()
         t += 1.0/f
